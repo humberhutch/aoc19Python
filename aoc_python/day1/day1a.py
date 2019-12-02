723a5c34afdc5b7e98bbc73b27d61ca21c58d9c1
G Hutchison <grant.hutchison@tdsb.on.ca>
2019-12-01 23:11:10 -0500

    modularized

--- aoc_python/day1/day1a.py
+++ aoc_python/day1/day1a.py
@@ -1,7 +1,13 @@
-f = open("day1.txt")
-fuel = f.readline()
-total_fuel = 0
-while (fuel != ""):
-    total_fuel += int(fuel)//3-2
+def calculate_fuel(data):
+    return int(data)//3-2
+
+def main():
+    f = open("day1.txt")
     fuel = f.readline()
-print (total_fuel)
+    total_fuel = 0
+    while (fuel != ""):
+        total_fuel += calculate_fuel(fuel)
+        fuel = f.readline()
+    print (total_fuel)
+
+main()
--- aoc_python/day1/day1b.py
+++ aoc_python/day1/day1b.py
@@ -1,15 +1,18 @@
-f = open("day1.txt")
-fuel = f.readline()
-total_fuel = 0
-while (fuel!=""):
-	sub_total = 0
-	next_fuel = int(fuel)//3-2
-	sub_total = next_fuel
-	
-	while (next_fuel >0):
-		next_fuel = int(next_fuel)//3-2
-		sub_total += next_fuel
-	total_fuel +=sub_total
+def calculate_fuel(data):
+    return int(data)//3-2
+
+def main():
+	f = open("day1.txt")
 	fuel = f.readline()
-	    
-print (total_fuel)
+	total_fuel = 0
+	while (fuel != ""):
+		sub_total=0
+		next_fuel = calculate_fuel(fuel)
+		while (next_fuel>0):
+			sub_total += next_fuel
+			next_fuel = calculate_fuel(next_fuel)
+		total_fuel +=sub_total
+		fuel = f.readline()
+	print (total_fuel)
+
+main()
